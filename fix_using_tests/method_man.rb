0d9f23039817cccd1ab33a4e7ca221ed33955190
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-09 12:01:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,7 @@
 # don't forget to add: require 'pry'
-require 'pry'
 
 def start_game(player1, player2)
-  binding.pry 
-  "Hello #{player1} & #{playar2}"
+  p "Hello #{player1} & #{player2}"
 end
 
 def play_game(player2)
