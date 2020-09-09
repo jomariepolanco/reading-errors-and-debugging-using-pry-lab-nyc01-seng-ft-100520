aed40571ff26940e28aab172867ea1ef45e9a109
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-09 12:10:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,4 @@
 # don't forget to add: require 'pry'
-require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -10,7 +9,7 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage(star_date)
-  puts state_log(date)
+def engage(date)
   date = generate_star_date
+  p state_log(date)
 end
